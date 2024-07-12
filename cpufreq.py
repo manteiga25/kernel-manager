722f95b24940cbf1ffcbe284e6700411fbb55224
manteiga25 <80206082+manteiga25@users.noreply.github.com>
2024-07-12 14:02:39 +0100

    Add support for change min and max freq per core

--- cpufreq.py
+++ cpufreq.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 from tkinter import LabelFrame
 from psutil import cpu_percent
 
@@ -87,24 +88,75 @@ def get_estado_core(num_core):
     try:
         with open(f"/sys/devices/system/cpu/cpu{num_core}/online", "r") as gov:
             print(num_core)
-            ret = gov.read().strip()
+            ret = int(gov.read().strip())
             print(ret)
-            if ret == "1":
-                print("yupii")
-                return 1
-            else:
-                return 0
+            return ret
+        #    if ret == "1":
+         #       print("yupii")
+          #      return 1
+           # else:
+            #    return 0
     except:
         print("err")
 
-def get_core_utilization(num_core):
+def get_core_table_freq(num_core):
     try:
-        with open(f"/sys/devices/system/cpu/cpu{num_core}/?", "w") as gov:
+        with open(f"/sys/devices/system/cpu/cpu{num_core}/cpufreq/scaling_available_frequencies", "r") as gov:
+            ret = gov.read().split(" ")
+            ret = [freq.strip() for freq in ret if freq.strip()]
+            return ret
+    except:
+        return "err"
+
+def get_core_min_freq(num_core):
+    try:
+        with open(f"/sys/devices/system/cpu/cpu{num_core}/cpufreq/min_freq?", "r") as gov:
             return gov.read().strip()
     except:
         return "err"
 
+def set_core_min_freq(num_core, freq):
+    try:
+        with open(f"/sys/devices/system/cpu/cpu{num_core}/cpufreq/min_freq?", "w") as gov:
+            gov.write(freq)
+    except:
+        return "err"
+
+def get_core_max_freq(num_core):
+    try:
+        with open(f"/sys/devices/system/cpu/cpu{num_core}/cpufreq/max_freq?", "r") as gov:
+            return gov.read().strip()
+    except:
+        return "err"
+
+def set_core_max_freq(num_core, freq):
+    try:
+        with open(f"/sys/devices/system/cpu/cpu{num_core}/cpufreq/max_freq?", "w") as gov:
+            gov.write(freq)
+    except:
+        return "err"
+
 class cpu_freq:
+
+    # tentativa de otimização
+    def get_min_cores_freq(num_cores):
+        pass
+
+    def muda_core_min_freq(self, core):
+        min_freq = self.lista_min_freq_core[core]
+        if min_freq > self.lista_max_freq_core[core]:
+            CTkMessagebox(title="Valor inválido", message="A Frequência mínima tem de ser menor que a frequência maxima.", icon="cancel")
+            self.lista_min_freq_core[core].set(get_core_min_freq(core))
+            return
+        set_core_min_freq(core, min_freq)
+
+    def muda_core_max_freq(self, core):
+        max_freq = self.lista_max_freq_core[core]
+        if max_freq < self.lista_min_freq_core[core]:
+            CTkMessagebox(title="Valor inválido", message="A Frequência maxima tem de ser maior que a frequência mínima.", icon="cancel")
+            self.lista_max_freq_core[core].set(get_core_max_freq(core))
+            return
+        set_core_min_freq(core, max_freq)
 
     def muda_governador(self, core):
             print(core)
@@ -148,6 +200,10 @@ class cpu_freq:
         self.lista_ok_preference = list(range(int(self.num_cores)))
         self.estado_core = list(range(int(self.num_cores)))
         self.state_core = list(range(int(self.num_cores)))
+        self.lista_min_freq_core = list(range(int(self.num_cores)))
+        self.lista_max_freq_core = list(range(int(self.num_cores)))
+        self.lista_min_freq_core_button = list(range(int(self.num_cores)))
+        self.lista_max_freq_core_button = list(range(int(self.num_cores)))
         for core in range(int(self.num_cores)):
             self.estado_core[core] = get_estado_core(core)
             frame_core = LabelFrame(self.frame_cpu_freq, text="core " + str(core), background='#212121', foreground="white")
@@ -162,23 +218,39 @@ class cpu_freq:
             self.lista_state_core[core].grid(column=1, row=1)
            # print(self.lista_state_core[core].get())
             #print(get_estado_core(core))
-            self.lista_core_utilization[core] = ctk.CTkLabel(frame_core, text="Utilization: " + get_core_utilization(core) + "%")
+            self.lista_core_utilization[core] = ctk.CTkLabel(frame_core)
             self.lista_core_utilization[core].grid(column=0, row=2, columnspan=1, padx=5)
             self.label_current_freq[core] = ctk.CTkLabel(frame_core)
             self.label_current_freq[core].grid(column=0, row=3, columnspan=2, sticky="w", padx=5)
-            ctk.CTkLabel(frame_core, text="Governador: ").grid(column=0, row=4)
+
+            ctk.CTkLabel(frame_core, text="Max freq: ").grid(column=0, row=4, pady=5)
+            self.lista_max_freq_core[core] = ctk.CTkComboBox(frame_core, values=get_core_table_freq(core))
+            self.lista_max_freq_core[core].set(get_core_max_freq(core))
+            self.lista_max_freq_core_button[core] = ctk.CTkButton(frame_core, text="salvar alteração" , command=lambda c=core: self.muda_core_max_freq(c))
+            self.lista_max_freq_core[core].grid(column=1, row=4, padx=10, pady=5)
+            self.lista_max_freq_core_button[core].grid(column=2, row=4, padx=10, pady=5)
+
+            ctk.CTkLabel(frame_core, text="Min freq: ").grid(column=0, row=5, pady=5)
+            self.lista_min_freq_core[core] = ctk.CTkComboBox(frame_core, values=get_core_table_freq(core))
+            self.lista_min_freq_core[core].set(get_core_min_freq(core))
+            self.lista_min_freq_core_button[core] = ctk.CTkButton(frame_core, text="salvar alteração" , command=lambda c=core: self.muda_core_min_freq(c))
+            self.lista_min_freq_core[core].grid(column=1, row=5, padx=10, pady=5)
+            self.lista_min_freq_core_button[core].grid(column=2, row=5, padx=10, pady=5)
+
+            ctk.CTkLabel(frame_core, text="Governador: ").grid(column=0, row=6, pady=5)
             self.lista_governadores[core] = ctk.CTkComboBox(frame_core, values=get_cpu_governors())
             self.lista_governadores[core].set(get_cpu_governor(core))
             self.lista_ok[core] = ctk.CTkButton(frame_core, text="salvar alteração" , command=lambda c=core: self.muda_governador(c))
          #   self.lista_governadores[core].bind("<ComboboxSelected>", lambda event: self.muda_governador(event, core))
-            self.lista_governadores[core].grid(column=1, row=4, padx=10)
-            self.lista_ok[core].grid(column=2, row=4, padx=10)
-            ctk.CTkLabel(frame_core, text="Preference: ").grid(column=0, row=5, pady=10)
+            self.lista_governadores[core].grid(column=1, row=6, padx=10, pady=5)
+            self.lista_ok[core].grid(column=2, row=6, padx=10, pady=5)
+
+            ctk.CTkLabel(frame_core, text="Preference: ").grid(column=0, row=7, pady=5)
             self.lista_energy_state[core] = ctk.CTkComboBox(frame_core, values=get_cpu_preferences())
             self.lista_energy_state[core].set(get_cpu_preference(core))
             self.lista_ok_preference[core] = ctk.CTkButton(frame_core, text="salvar alteração" , command=lambda c=core: self.muda_preferencia(c))
-            self.lista_energy_state[core].grid(column=1, row=5, padx=10, pady=10)
-            self.lista_ok_preference[core].grid(column=2, row=5, padx=10, pady=10)
+            self.lista_energy_state[core].grid(column=1, row=7, padx=10, pady=5)
+            self.lista_ok_preference[core].grid(column=2, row=7, padx=10, pady=5)
         self.lista_state_core[0].configure(state="disabled") # o kernel não permite modificar o estado desse nucleo para o funcionamento do sistema
         self.update_current_freq_label()
 
