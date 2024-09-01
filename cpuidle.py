45af2fe637c8b70af314288558feb0e3744405fd
manteiga25 <80206082+manteiga25@users.noreply.github.com>
2024-09-01 12:22:51 +0100

    Cpuidle: layout fixes

--- cpuidle.py
+++ cpuidle.py
@@ -21,16 +21,10 @@ def get_cpu_idle_governors():
 
 def set_cpu_idle_governor(governador):
     try:
-        with open(f"/sys/devices/system/cpu/cpuidle/current_governor_ro", "w") as gov:
-            gov.write(governador)
-    except:
-        return "err"
-
-    try:
         with open(f"/sys/devices/system/cpu/cpuidle/current_governor", "w") as gov:
             gov.write(governador)
     except:
-        return "err"
+        print("idbciusd")
 
 def set_cpu_clocksource_mode(mode):
     try:
@@ -77,23 +71,23 @@ class cpu_idle:
         self.cpu_clocksource_f = LabelFrame(master=self.frame_cpu_idle_glob, text="Clocksource", background='#212121', foreground="white")
         self.cpu_clocksource_f.pack(fill="both", expand=True, pady=20)
         ctk.CTkLabel(self.cpu_clocksource_f, text="O clocksource é responsável por manter uma contagem de tempo e gerenciar os ticks do kernel.", wraplength=700, justify="center").grid(column=1, row=0, columnspan=6)
-        ctk.CTkLabel(self.cpu_clocksource_f, text="Clocksource mode: ").grid(column=0, row=2, pady=5)
+        ctk.CTkLabel(self.cpu_clocksource_f, text="Clocksource mode: ").grid(column=0, row=2, pady=5, padx=5)
         self.modos_clocksource = ctk.CTkComboBox(self.cpu_clocksource_f, values=get_cpu_clocksource_modes())
         self.modos_clocksource.set(get_cpu_clocksource_mode())
         ok = ctk.CTkButton(self.cpu_clocksource_f, text="salvar alteração" , command=self.muda_modo)
-        self.modos_clocksource.grid(column=1, row=2, padx=10, pady=5)
-        ok.grid(column=2, row=2, padx=10, pady=5)
+        self.modos_clocksource.grid(column=1, row=2, padx=5, pady=5)
+        ok.grid(column=2, row=2, padx=5, pady=5)
 
     def cpu_idle_frame(self):
         self.frame_cpu_idle = LabelFrame(self.frame_cpu_idle_glob, width=880, height=380, text="CPU Idle", background='#212121', foreground="white")
         self.frame_cpu_idle.pack(fill="both", expand=True)
         ctk.CTkLabel(self.frame_cpu_idle, text="CPU idle é um recurso de gerenciamento de energia que permite desabilitar partes da CPU que estão ociosas quando nenhum programa esta a ser executado ou em carga baixa para economizar energia.", wraplength=700, justify="center").grid(column=1, row=0, columnspan=6)
-        ctk.CTkLabel(self.frame_cpu_idle, text="Governador idle: ").grid(column=0, row=2, pady=5)
+        ctk.CTkLabel(self.frame_cpu_idle, text="Governador idle: ").grid(column=0, row=2, pady=5, padx=5)
         self.governadores_idle = ctk.CTkComboBox(self.frame_cpu_idle, values=get_cpu_idle_governors())
         self.governadores_idle.set(get_cpu_idle_governor())
         ok = ctk.CTkButton(self.frame_cpu_idle, text="salvar alteração" , command=self.muda_governador)
-        self.governadores_idle.grid(column=1, row=2, padx=10, pady=5)
-        ok.grid(column=2, row=2, padx=10, pady=5)
+        self.governadores_idle.grid(column=1, row=2, padx=5, pady=5)
+        ok.grid(column=2, row=2, padx=5, pady=5)
 
     def rende_cpu_idle(self, menu):
         self.menu = menu
